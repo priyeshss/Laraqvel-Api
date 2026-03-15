5a39baa59cb728539ad95a810b85f5fedfc182a8
Priyesh <surtipriyesh113@gmail.com>
2026-03-15 23:42:21 +0530

    first commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthController extends Controller
+{
+
+public function register(Request $request){
+    $user = User::create(
+        [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]
+    );
+    return response()->json(
+        [
+            "message" => "User Registered Scuccessfully"
+        ]
+    );
+}
+
+public function login(Request $request){
+    if(!Auth::attempt($request->only('email','password')))
+        {
+        return response->json(
+            [ "message"=>"Invalid Credentials" ],401
+            );
+        }
+    $user = Auth::user();
+    $token = $user-> createToken('api-token')->plainTextToken;
+    return response()->json([
+        "token"=>$token
+    ]);
+}
+
+}
---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);

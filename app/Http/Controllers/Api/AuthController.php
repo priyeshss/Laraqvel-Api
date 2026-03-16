97a4b7fef098d8e87b7dcdd09cabbd7fe634c9f8
Priyesh <surtipriyesh113@gmail.com>
2026-03-16 23:24:44 +0530

    second commit

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,32 +12,40 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
 
-public function register(Request $request){
-    $user = User::create(
-        [
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
-        ]
-    );
-    return response()->json(
-        [
-            "message" => "User Registered Scuccessfully"
-        ]
-    );
+public function register(Request $request)
+{
+    $user = User::create([
+        'name'=>$request->name,
+        'email'=>$request->email,
+        'password'=>Hash::make($request->password)
+    ]);
+
+    return response()->json([
+        "message"=>"User registered successfully"
+    ]);
 }
 
 public function login(Request $request){
     if(!Auth::attempt($request->only('email','password')))
         {
-        return response->json(
+        return response()->json(
             [ "message"=>"Invalid Credentials" ],401
             );
         }
     $user = Auth::user();
+    $user->tokens()->delete();
     $token = $user-> createToken('api-token')->plainTextToken;
     return response()->json([
         "token"=>$token
+    ]);
+}
+
+public function logout(Request $request)
+{
+    $request->user()->currentAccessToken()->delete(); //current device
+
+    return response()->json([
+        "message" => "Logged out successfully"
     ]);
 }
 
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,19 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+
+Route::middleware('auth:sanctum')->group(function(){
+
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('products', ProductController::class);
+
+});
